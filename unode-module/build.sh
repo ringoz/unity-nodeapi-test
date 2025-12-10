045ec59e8219137bade7c9a224e466372ccf8379
Vladimir Davidovich <thy.ringo@gmail.com>
2025-12-10 18:14:43 +0300

    unity batchmode build

--- unode-module/build.sh
+++ unode-module/build.sh
@@ -1,15 +1,19 @@
-#!/bin/sh
+#!/bin/bash
+set -e
 
-out=$(pwd)/Packages/net.ringoz.unity.nodeapi/Runtime/Unity.NodeApi.d.ts
+UNITY_VERSION=$(head -n 1 ProjectSettings/ProjectVersion.txt | cut -d ' ' -f 2)
+/Applications/Unity/Hub/Editor/$UNITY_VERSION/Unity.app/Contents/MacOS/Unity -quit -batchmode -nographic -logFile - -projectPath "." -buildOSXUniversalPlayer "Build.app"
+
+DTS_OUT=$(pwd)/Packages/net.ringoz.unity.nodeapi/Runtime/Unity.NodeApi.d.ts
 pushd Build_BackUpThisFolder_ButDontShipItWithYourGame/Managed/
 
 # enumerate all .dll files in the current directory and put them to semicolon-separated list
-refs=$(find . -name "*.dll" -type f -print0 | sed 's/.\//;/g')
+NET_REFS=$(find . -name "*.dll" -type f -print0 | sed 's/.\//;/g')
 
 dotnet ~/Documents/node-api-dotnet/out/bin/Release/NodeApi.Generator/net10.0/osx-arm64/Microsoft.JavaScript.NodeApi.Generator.dll \
   --assembly "Unity.NodeApi.dll" \
-  --reference "$refs" \
-  --typedefs "$out" \
+  --reference "$NET_REFS" \
+  --typedefs "$DTS_OUT" \
   --framework "netstandard2.1"
 
 popd
