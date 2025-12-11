ac5816f1154aaa3ea401e729f2e4ae0700748cf8
Vladimir Davidovich <thy.ringo@gmail.com>
2025-12-11 17:01:14 +0300

    remove build.cmd

--- unode-module/build.cmd
+++
@@ -1,2 +0,0 @@
-mklink /J "Build_BackUpThisFolder_ButDontShipItWithYourGame" "Build.app/unode-module_BackUpThisFolder_ButDontShipItWithYourGame"
-"C:\Program Files\Git\bin\bash.exe" build.sh %*
--- unode-module/build.sh
+++ unode-module/build.sh
@@ -5,7 +5,9 @@ UNITY_VERSION=$(head -n 1 ProjectSettings/ProjectVersion.txt | cut -d ' ' -f 2)
 case "$OSTYPE" in
   msys*)
     RID=win-x64
+    mv --force "Build_BackUpThisFolder_ButDontShipItWithYourGame" "Build.app/unode-module_BackUpThisFolder_ButDontShipItWithYourGame"
     "C:/Program Files/Unity/Hub/Editor/$UNITY_VERSION/Editor/Unity.com" -quit -batchmode -nographic -logFile - -projectPath "." -buildWindows64Player "Build.app/unode-module.exe"
+    mv --force "Build.app/unode-module_BackUpThisFolder_ButDontShipItWithYourGame" "Build_BackUpThisFolder_ButDontShipItWithYourGame"
     ;;
   darwin*)
     RID=osx-arm64
@@ -17,5 +19,4 @@ DTS_OUT=$(pwd)/Packages/net.ringoz.unity.nodeapi/Runtime/Unity.NodeApi.d.ts
 cd Build_BackUpThisFolder_ButDontShipItWithYourGame/Managed/
 
 NET_REFS=$(find . -name "*.dll" -type f -print0 | sed 's/.\//;/g')
-echo $NET_REFS
-../../../../../node-api-dotnet/out/bin/Release/NodeApi.Generator/net10.0/$RID/Microsoft.JavaScript.NodeApi.Generator --assembly "Unity.NodeApi.dll" --reference "$NET_REFS" --typedefs "$DTS_OUT" --framework "netstandard2.1"
+../../../../node-api-dotnet/out/bin/Release/NodeApi.Generator/net10.0/$RID/Microsoft.JavaScript.NodeApi.Generator --assembly "Unity.NodeApi.dll" --reference "$NET_REFS" --typedefs "$DTS_OUT" --framework "netstandard2.1"
