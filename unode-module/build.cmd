bad7822f661ea9fe2dba057dcc7c3b21acf439c7
Vladimir Davidovich <thy.ringo@gmail.com>
2025-12-11 16:41:09 +0300

    build.cmd

---
+++ unode-module/build.cmd
@@ -0,0 +1,2 @@
+mklink /J "Build_BackUpThisFolder_ButDontShipItWithYourGame" "Build.app/unode-module_BackUpThisFolder_ButDontShipItWithYourGame"
+"C:\Program Files\Git\bin\bash.exe" build.sh %*
--- unode-module/build.sh
+++ unode-module/build.sh
@@ -2,18 +2,20 @@
 set -e
 
 UNITY_VERSION=$(head -n 1 ProjectSettings/ProjectVersion.txt | cut -d ' ' -f 2)
-/Applications/Unity/Hub/Editor/$UNITY_VERSION/Unity.app/Contents/MacOS/Unity -quit -batchmode -nographic -logFile - -projectPath "." -buildOSXUniversalPlayer "Build.app"
+case "$OSTYPE" in
+  msys*)
+    RID=win-x64
+    "C:/Program Files/Unity/Hub/Editor/$UNITY_VERSION/Editor/Unity.com" -quit -batchmode -nographic -logFile - -projectPath "." -buildWindows64Player "Build.app/unode-module.exe"
+    ;;
+  darwin*)
+    RID=osx-arm64
+    /Applications/Unity/Hub/Editor/$UNITY_VERSION/Unity.app/Contents/MacOS/Unity -quit -batchmode -nographic -logFile - -projectPath "." -buildOSXUniversalPlayer "Build.app"
+    ;;
+esac
 
 DTS_OUT=$(pwd)/Packages/net.ringoz.unity.nodeapi/Runtime/Unity.NodeApi.d.ts
-pushd Build_BackUpThisFolder_ButDontShipItWithYourGame/Managed/
+cd Build_BackUpThisFolder_ButDontShipItWithYourGame/Managed/
 
-# enumerate all .dll files in the current directory and put them to semicolon-separated list
 NET_REFS=$(find . -name "*.dll" -type f -print0 | sed 's/.\//;/g')
-
-dotnet ~/Documents/node-api-dotnet/out/bin/Release/NodeApi.Generator/net10.0/osx-arm64/Microsoft.JavaScript.NodeApi.Generator.dll \
-  --assembly "Unity.NodeApi.dll" \
-  --reference "$NET_REFS" \
-  --typedefs "$DTS_OUT" \
-  --framework "netstandard2.1"
-
-popd
+echo $NET_REFS
+../../../../../node-api-dotnet/out/bin/Release/NodeApi.Generator/net10.0/$RID/Microsoft.JavaScript.NodeApi.Generator --assembly "Unity.NodeApi.dll" --reference "$NET_REFS" --typedefs "$DTS_OUT" --framework "netstandard2.1"
