fa0658f0ab596d51f6dbca5d5e681e3ecfd7e75c
Vladimir Davidovich <thy.ringo@gmail.com>
2025-12-18 14:49:11 +0300

    Build.web

--- unode-module/build.sh
+++ unode-module/build.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 set -e
 
+DTS_OUT=$(pwd)/Packages/net.ringoz.unity.nodeapi/Runtime/Unity.NodeApi.d.ts
 UNITY_VERSION=$(head -n 1 ProjectSettings/ProjectVersion.txt | cut -d ' ' -f 2)
+
 case "$OSTYPE" in
   msys*)
-    RID=win-x64
-    mv --force "Build_BackUpThisFolder_ButDontShipItWithYourGame" "Build.app/UnityPlayer_BackUpThisFolder_ButDontShipItWithYourGame" || true
-    "C:/Program Files/Unity/Hub/Editor/$UNITY_VERSION/Editor/Unity.com" -quit -batchmode -nographic -logFile - -projectPath "." -buildWindows64Player "Build.app/UnityPlayer.exe"
-    mv --force "Build.app/UnityPlayer_BackUpThisFolder_ButDontShipItWithYourGame" "Build_BackUpThisFolder_ButDontShipItWithYourGame"
+    GENTS=$(pwd)/../../node-api-dotnet/out/bin/Release/NodeApi.Generator/net10.0/win-x64/Microsoft.JavaScript.NodeApi.Generator
+    UNITY="C:/Program Files/Unity/Hub/Editor/$UNITY_VERSION/Editor/Unity.com"
+    #$UNITY -quit -batchmode -nographic -logFile - -projectPath "." -buildWindows64Player "Build.app/UnityPlayer.exe"
+    #cd Build.app/UnityPlayer_BackUpThisFolder_ButDontShipItWithYourGame
     ;;
   darwin*)
-    RID=osx-arm64
-    /Applications/Unity/Hub/Editor/$UNITY_VERSION/Unity.app/Contents/MacOS/Unity -quit -batchmode -nographic -logFile - -projectPath "." -buildOSXUniversalPlayer "Build.app"
+    GENTS=$(pwd)/../../node-api-dotnet/out/bin/Release/NodeApi.Generator/net10.0/osx-arm64/Microsoft.JavaScript.NodeApi.Generator
+    UNITY=/Applications/Unity/Hub/Editor/$UNITY_VERSION/Unity.app/Contents/MacOS/Unity
+    #$UNITY -quit -batchmode -nographic -logFile - -projectPath "." -buildOSXUniversalPlayer "Build.app"
+    #cd Build_BackUpThisFolder_ButDontShipItWithYourGame/Managed/
     ;;
 esac
 
-DTS_OUT=$(pwd)/Packages/net.ringoz.unity.nodeapi/Runtime/Unity.NodeApi.d.ts
-cd Build_BackUpThisFolder_ButDontShipItWithYourGame/Managed/
+$UNITY -quit -batchmode -nographic -logFile - -projectPath "." -activeBuildProfile "Assets/Settings/Build Profiles/WebDev.asset" -build "Build.web"
+cd Library/Bee/artifacts/WebGL/ManagedStripped/
 
 NET_REFS=$(find . -name "*.dll" -type f -print0 | sed 's/.\//;/g')
-../../../../node-api-dotnet/out/bin/Release/NodeApi.Generator/net10.0/$RID/Microsoft.JavaScript.NodeApi.Generator --assembly "Unity.NodeApi.dll" --reference "$NET_REFS" --typedefs "$DTS_OUT" --framework "netstandard2.1"
+$GENTS --assembly "Unity.NodeApi.dll" --reference "$NET_REFS" --typedefs "$DTS_OUT" --framework "netstandard2.1"
