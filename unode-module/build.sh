6b11bf3be8404b78ecce4291512e55e0592aac46
Vladimir Davidovich <thy.ringo@gmail.com>
2025-12-18 21:50:44 +0300

    [win] fix build

--- unode-module/build.sh
+++ unode-module/build.sh
@@ -8,19 +8,19 @@ case "$OSTYPE" in
   msys*)
     GENTS=$(pwd)/../../node-api-dotnet/out/bin/Release/NodeApi.Generator/net10.0/win-x64/Microsoft.JavaScript.NodeApi.Generator
     UNITY="C:/Program Files/Unity/Hub/Editor/$UNITY_VERSION/Editor/Unity.com"
-    #$UNITY -quit -batchmode -nographic -logFile - -projectPath "." -buildWindows64Player "Build.app/UnityPlayer.exe"
+    #"$UNITY" -quit -batchmode -nographic -logFile - -projectPath "." -buildWindows64Player "Build.app/UnityPlayer.exe"
     #cd Build.app/UnityPlayer_BackUpThisFolder_ButDontShipItWithYourGame
     ;;
   darwin*)
     GENTS=$(pwd)/../../node-api-dotnet/out/bin/Release/NodeApi.Generator/net10.0/osx-arm64/Microsoft.JavaScript.NodeApi.Generator
     UNITY=/Applications/Unity/Hub/Editor/$UNITY_VERSION/Unity.app/Contents/MacOS/Unity
-    #$UNITY -quit -batchmode -nographic -logFile - -projectPath "." -buildOSXUniversalPlayer "Build.app"
+    #"$UNITY" -quit -batchmode -nographic -logFile - -projectPath "." -buildOSXUniversalPlayer "Build.app"
     #cd Build_BackUpThisFolder_ButDontShipItWithYourGame/Managed/
     ;;
 esac
 
-$UNITY -quit -batchmode -nographic -logFile - -projectPath "." -activeBuildProfile "Assets/Settings/Build Profiles/WebDev.asset" -build "Build.web"
+"$UNITY" -quit -batchmode -nographic -logFile - -projectPath "." -activeBuildProfile "Assets/Settings/Build Profiles/WebDev.asset" -build "Build.web"
 cd Library/Bee/artifacts/WebGL/ManagedStripped/
 
 NET_REFS=$(find . -name "*.dll" -type f -print0 | sed 's/.\//;/g')
-$GENTS --assembly "Unity.NodeApi.dll" --reference "$NET_REFS" --typedefs "$DTS_OUT" --framework "netstandard2.1"
+"$GENTS" --assembly "Unity.NodeApi.dll" --reference "$NET_REFS" --typedefs "$DTS_OUT" --framework "netstandard2.1"
