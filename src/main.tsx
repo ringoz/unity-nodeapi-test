3bb1155d73b0338dde395ce39ae44a928cfa7036
Vladimir Davidovich <thy.ringo@gmail.com>
2025-12-20 12:57:27 +0300

    publicDir: './unode-module/Build.web/Build'

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
-//import { hello } from '../unode-module/index.ts';
 
-//hello('Hello world!');
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('html-root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
