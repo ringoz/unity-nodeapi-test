cdbce64669b7a2b36e977a430c8c30e15cf628d6
Vladimir Davidovich <thy.ringo@gmail.com>
2025-12-21 21:26:51 +0300

    lint ignores

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { hello } from 'net.ringoz.unity.nodeapi';
 console.log('Hello from Node.js');
 hello('Hello world!');
 
-let running = true;
+const running = true;
 while (running) {
   console.log(Date.now());
   queueMicrotask(() => console.log('a'));
