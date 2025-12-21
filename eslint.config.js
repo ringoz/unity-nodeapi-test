8eb6de2f1935c1ca05b882e0301ee166e828cea9
Vladimir Davidovich <thy.ringo@gmail.com>
2025-12-21 21:57:28 +0300

    lint ignores

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', '**/Build*', '**/Library']),
+  globalIgnores(['dist', 'unode-module']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
