c14debf2d6085a9c383f5bd11f696ecc56ffa1d2
Vladimir Davidovich <thy.ringo@gmail.com>
2025-12-23 00:43:12 +0300

    net.ringoz.unity.nodeapi/react

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'net.ringoz.unity.nodeapi/react'
 import App from './App.tsx'
 
 import { rendererPackageName, rendererVersion } from 'net.ringoz.unity.nodeapi';
 console.log(`${rendererPackageName} v${rendererVersion}`);
 
 createRoot(document.getElementById('html-root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App />
 );
