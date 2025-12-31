2ab6768c098e0ac10418c864752de1d5e587ead1
Vladimir Davidovich <thy.ringo@gmail.com>
2025-12-31 23:48:42 +0300

    roll deps

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,16 @@ function App() {
   return (
     <>
       <sphere name="sphere1">
-        <transform localScale="3 3 3" position="1 2 3" />
+        <transform localScale="2 2 2" localPosition="1 2 3" />
+        <object>
+          <transform localScale="0.5 0.5 0.5" />
+          <cylinder>
+            <transform localPosition="0 1 1" />
+          </cylinder>
+          <cube>
+            <transform localPosition="-1 -1 -1" />
+          </cube>
+        </object>
       </sphere>
     </>
   )
