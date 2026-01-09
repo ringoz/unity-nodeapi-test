1df7ea66897dd27bb3a7c617720015b768b71f8a
Vladimir Davidovich <thy.ringo@gmail.com>
2026-01-09 17:47:20 +0300

    nothing

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,6 @@ import { Element } from 'net.ringoz.unity.nodeapi';
 import { createRoot } from 'net.ringoz.unity.nodeapi/react';
 import App from './App.tsx';
 
-createRoot(Element.search("ReactRoot")).render(
+createRoot(Element.search("React Root")).render(
   <App />
 );
