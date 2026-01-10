362a76938b4292d9f521188590d9bc4e8e5d2548
Vladimir Davidovich <thy.ringo@gmail.com>
2026-01-10 13:15:58 +0300

    //#region generated

--- unode-module/index.ts
+++ unode-module/index.ts
@@ -1,2 +1,15 @@
+import type { Component, Vector3 } from './Packages/net.ringoz.unity.nodeapi/react';
+import { intrinsic } from './Packages/net.ringoz.unity.nodeapi/react';
+
 export * from './Packages/net.ringoz.unity.nodeapi';
 export * from './Packages/net.ringoz.unity.nodeapi/react';
+
+//#region generated
+
+export interface Transform extends Component {
+  localScale: Vector3;
+  localPosition: Vector3;
+}
+export const Transform = intrinsic<Transform>("Transform");
+
+//#endregion generated
