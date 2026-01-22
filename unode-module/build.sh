30f08f20fcb8bed59e8844aae0955abc1c817833
Vladimir Davidovich <thy.ringo@gmail.com>
2026-01-22 11:20:03 +0300

    fix typo nographics

--- unode-module/build.sh
+++ unode-module/build.sh
@@ -8,18 +8,18 @@ case "$OSTYPE" in
   msys*)
     UNITY_EXE="C:/Program Files/Unity/Hub/Editor/$UNITY_VER/Editor/Unity.com"
     UNITY_NET="C:/Program Files/Unity/Hub/Editor/$UNITY_VER/Editor/Data"
-    #"$UNITY_EXE" -quit -batchmode -nographic -logFile - -projectPath "." -buildWindows64Player "Build.app/UnityPlayer.exe"
+    #"$UNITY_EXE" -quit -batchmode -nographics -logFile - -projectPath "." -buildWindows64Player "Build.app/UnityPlayer.exe"
     #cd Build.app/UnityPlayer_BackUpThisFolder_ButDontShipItWithYourGame/Managed/
     ;;
   darwin*)
     UNITY_EXE=/Applications/Unity/Hub/Editor/$UNITY_VER/Unity.app/Contents/MacOS/Unity
     UNITY_NET=/Applications/Unity/Hub/Editor/$UNITY_VER/Unity.app/Contents/Resources/Scripting
-    #"$UNITY_EXE" -quit -batchmode -nographic -logFile - -projectPath "." -buildOSXUniversalPlayer "Build.app"
+    #"$UNITY_EXE" -quit -batchmode -nographics -logFile - -projectPath "." -buildOSXUniversalPlayer "Build.app"
     #cd Build_BackUpThisFolder_ButDontShipItWithYourGame/Managed/
     ;;
 esac
 
-"$UNITY_EXE" -quit -batchmode -nographic -logFile - -projectPath "." -activeBuildProfile "Assets/Settings/Build Profiles/WebDev.asset" -build "Build.web"
+"$UNITY_EXE" -quit -batchmode -nographics -logFile - -projectPath "." -activeBuildProfile "Assets/Settings/Build Profiles/WebDev.asset" -build "Build.web"
 cd Library/Bee/artifacts/WebGL/ManagedStripped/
 
 NET_REFS=$(find . -name "*.dll" -type f -print0 | sed 's/.\//;/g')
