c6106ec783db8e7aaac9bb17d71ec3bd821e5c2d
Vladimir Davidovich <thy.ringo@gmail.com>
2026-01-26 13:21:55 +0300

    #nullable enable

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot, Node } from 'unode-module';
 import App from './App.tsx';
 
-createRoot(Node.search("React Root")).render(
+createRoot(Node.search("React Root")!).render(
   <App />
 );
